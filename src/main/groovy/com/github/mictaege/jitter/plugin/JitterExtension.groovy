e6d3d7fbb67e9253fb1a78afb8973b350de2e651
Michael Täge <mictaege@googlemail.com>
2017-11-16 07:42:21 +0100

    #7 Exclude configured source sets from jitter processing

--- src/main/groovy/com/github/mictaege/jitter/plugin/JitterExtension.groovy
+++ src/main/groovy/com/github/mictaege/jitter/plugin/JitterExtension.groovy
@@ -8,4 +8,7 @@ class JitterExtension {
     /** Java source code compliance level (1,2,3,4,5, 6, 7 or 8). (default: 8) */
     int compliance = 8
 
+    /** List of excluded source sets. */
+    String[] exclude = []
+
 }
--- src/main/groovy/com/github/mictaege/jitter/plugin/JitterPlugin.groovy
+++ src/main/groovy/com/github/mictaege/jitter/plugin/JitterPlugin.groovy
@@ -1,6 +1,6 @@
 package com.github.mictaege.jitter.plugin
 
-import com.github.mictaege.spoon_gradle_plugin.SpoonTask
+import com.github.mictaege.spoon_gradle_plugin.SpoonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.language.jvm.tasks.ProcessResources
@@ -13,28 +13,29 @@ class JitterPlugin implements Plugin<Project>  {
     void apply(final Project project) {
         project.extensions.create "jitter", JitterExtension
 
+
         project.configure(project) {
             apply plugin: 'java'
             apply plugin: 'spoon'
+
+            project.spoon.lazyExtensions = { ->
+                    def spoonExt = new SpoonExtension()
+                    spoonExt.buildOnlyOutdatedFiles = true
+                    spoonExt.processors = [
+                            'com.github.mictaege.jitter.plugin.AlterClassProcessor',
+                            'com.github.mictaege.jitter.plugin.ForkMethodProcessor',
+                            'com.github.mictaege.jitter.plugin.OnlyIfPackageProcessor',
+                            'com.github.mictaege.jitter.plugin.OnlyIfClassProcessor',
+                            'com.github.mictaege.jitter.plugin.OnlyIfFieldProcessor',
+                            'com.github.mictaege.jitter.plugin.OnlyIfMethodProcessor',
+                            'com.github.mictaege.jitter.plugin.OnlyIfConstructorProcessor'
+                    ]
+                    spoonExt.compliance = project.jitter.compliance
+                    spoonExt.exclude = project.jitter.exclude
+                    return spoonExt
+                }
+
         }
-
-        project.afterEvaluate({
-
-            project.tasks.withType(SpoonTask) {
-                it.processors = [
-                        'com.github.mictaege.jitter.plugin.AlterClassProcessor',
-                        'com.github.mictaege.jitter.plugin.ForkMethodProcessor',
-                        'com.github.mictaege.jitter.plugin.OnlyIfPackageProcessor',
-                        'com.github.mictaege.jitter.plugin.OnlyIfClassProcessor',
-                        'com.github.mictaege.jitter.plugin.OnlyIfFieldProcessor',
-                        'com.github.mictaege.jitter.plugin.OnlyIfMethodProcessor',
-                        'com.github.mictaege.jitter.plugin.OnlyIfConstructorProcessor'
-                ]
-
-                it.compliance = project.jitter.compliance
-            }
-
-        })
 
         project.afterEvaluate({
             project.jitter.flavours.each {f ->
