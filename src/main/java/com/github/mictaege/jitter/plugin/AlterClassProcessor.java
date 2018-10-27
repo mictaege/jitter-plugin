9d9153a8a105e052eec67a05c3ea74e6277a298c
Michael Täge <mictaege@googlemail.com>
2018-10-27 23:51:50 +0200

    #37 Processing @Alter generates fields with duplicate visibility modifiers

--- src/main/java/com/github/mictaege/jitter/plugin/AlterClassProcessor.java
+++ src/main/java/com/github/mictaege/jitter/plugin/AlterClassProcessor.java
@@ -3,7 +3,11 @@ package com.github.mictaege.jitter.plugin;
 import static com.github.mictaege.jitter.plugin.JitterUtil.active;
 import static com.github.mictaege.jitter.plugin.JitterUtil.anyFlavour;
 import static com.github.mictaege.jitter.plugin.JitterUtil.log;
+import static java.util.Arrays.asList;
 import static java.util.Optional.ofNullable;
+import static spoon.reflect.declaration.ModifierKind.PRIVATE;
+import static spoon.reflect.declaration.ModifierKind.PROTECTED;
+import static spoon.reflect.declaration.ModifierKind.PUBLIC;
 
 import java.util.Optional;
 
@@ -12,6 +16,7 @@ import com.github.mictaege.jitter.api.Alter;
 import spoon.processing.AbstractAnnotationProcessor;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.CtTypeMember;
 import spoon.reflect.declaration.ModifierKind;
 
 public class AlterClassProcessor extends AbstractAnnotationProcessor<Alter, CtClass<?>> {
@@ -38,11 +43,30 @@ public class AlterClassProcessor extends AbstractAnnotationProcessor<Alter, CtCl
                 final CtType<?> clone = altType.get().clone();
                 clone.setVisibility(visibility);
                 clone.setSimpleName(simpleName);
-
+                fixModifiers(altType.get(), clone);
                 clazz.getPackage().addType(clone);
             } else {
                 log().error("The given alternative class " + altClass + " could not be found");
             }
+        }
+    }
+
+    private void fixModifiers(final CtType<?> altType, final CtType<?> clone) {
+        clone.getFields().forEach(f -> fixModifier(f, altType));
+        clone.getMethods().forEach(m -> fixModifier(m, altType));
+    }
+
+    private void fixModifier(final CtTypeMember cloneMember, final CtType<?> altType) {
+        final boolean duplicateVisibility =
+                cloneMember.getExtendedModifiers().stream()
+                        .filter(m -> asList(PRIVATE, PUBLIC, PROTECTED).contains(m.getKind()))
+                        .count() > 1;
+        if (duplicateVisibility) {
+            log().info("Duplicate visibility modifiers found after spoon processing. Try to fix " + altType.getSimpleName() + "#" + cloneMember.getSimpleName());
+            altType.getTypeMembers().stream()
+                    .filter(a -> a.getSimpleName().equals(cloneMember.getSimpleName()))
+                    .findFirst()
+                    .ifPresent(a -> cloneMember.setExtendedModifiers(a.getExtendedModifiers()));
         }
     }
 
